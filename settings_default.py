d3c557d621782a192e0acbbaf4a39ef59f32c6d1
Calin Crisan <ccrisan@gmail.com>
2013-10-13 17:52:18 +0300

    conf and run paths are now by default absolute

--- settings_default.py
+++ settings_default.py
@@ -5,8 +5,8 @@ import sys
 
 
 PROJECT_PATH = os.path.dirname(sys.argv[0])
-CONF_PATH = os.path.join(PROJECT_PATH, 'conf')
-RUN_PATH = os.path.join(PROJECT_PATH, 'run')
+CONF_PATH = os.path.abspath(os.path.join(PROJECT_PATH, 'conf'))
+RUN_PATH = os.path.abspath(os.path.join(PROJECT_PATH, 'run'))
 
 LOG_LEVEL = logging.DEBUG
 DEBUG = (LOG_LEVEL == logging.DEBUG)
