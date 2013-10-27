6d983b5debc99596e26df20a465d58b088489530
Calin Crisan <ccrisan@gmail.com>
2013-10-27 12:39:32 +0200

    added license information

--- settings_default.py
+++ settings_default.py
@@ -3,23 +3,37 @@ import logging
 import os.path
 import sys
 
-
+# you normally don't have to change these 
 PROJECT_PATH = os.path.dirname(sys.argv[0])
-CONF_PATH = os.path.abspath(os.path.join(PROJECT_PATH, 'conf'))
-RUN_PATH = os.path.abspath(os.path.join(PROJECT_PATH, 'run'))
-
-REPO = ('ccrisan', 'motioneye')
-
-LOG_LEVEL = logging.DEBUG
-
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
-
 STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
+
+# static files (.css, .js etc) are served at this root url
 STATIC_URL = '/static/'
 
+# path to the config directory; must be writable
+CONF_PATH = os.path.abspath(os.path.join(PROJECT_PATH, 'conf'))
+
+# logs, pid files and other output files go here
+RUN_PATH = os.path.abspath(os.path.join(PROJECT_PATH, 'run'))
+
+# repository details for software updating
+REPO = ('ccrisan', 'motioneye')
+
+# set to logging.DEBUG for verbose output
+LOG_LEVEL = logging.INFO
+
+# set to 127.0.0.1 to restrict access to localhost
 LISTEN = '0.0.0.0'
+
+# change the port according to your requirements/restrictions
 PORT = 8765
 
+# interval in seconds at which motionEye checks if motion is running
 MOTION_CHECK_INTERVAL = 10
+
+# interval in seconds at which the janitor is called to remove old images and movies
 CLEANUP_INTERVAL = 43200
+
+# timeout in seconds to wait for responses when contacting a remote server
 REMOTE_REQUEST_TIMEOUT = 10
--- src/handlers.py
+++ src/handlers.py
@@ -1,11 +1,24 @@
 
+# Copyright (c) 2013 Calin Crisan
+# This file is part of motionEye.
+#
+# motionEye is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
 import base64
 import json
 import logging
-import os
-import sys
 
-from tornado import ioloop
 from tornado.web import RequestHandler, HTTPError, asynchronous
 
 import config
