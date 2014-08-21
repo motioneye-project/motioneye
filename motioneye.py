cc3406e10fa2b854d0c5c51ca490749d8d4273b2
Calin Crisan <ccrisan@gmail.com>
2014-08-21 19:49:33 +0300

    version bump to 0.14

--- motioneye.py
+++ motioneye.py
@@ -29,7 +29,7 @@ import settings
 
 sys.path.append(os.path.join(getattr(settings, 'PROJECT_PATH', os.path.dirname(sys.argv[0])), 'src'))
 
-VERSION = '0.13'
+VERSION = '0.14'
 
 
 def _configure_settings():
