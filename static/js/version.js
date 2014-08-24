fb52b583dc4ef66f1383f1650d1173afd2990cfa
Calin Crisan <ccrisan@gmail.com>
2014-08-24 20:16:58 +0300

    added a special /version/ uri

---
+++ static/js/version.js
@@ -0,0 +1,6 @@
+
+$(window).load(function () {
+    if (window.parent && window.parent.postMessage) {
+        window.parent.postMessage({'hostname': hostname, 'version': version, 'url': window.location.href.replace('version/', '')}, '*');
+    }
+});
