09eca9d2d86182470fb251175b4dece0f0b53e2b
Bob-PC\Bob <bob.bumsuk.lee@gmail.com>
2019-01-12 10:51:09 +1300

    correct inputs to clean_cloud

--- motioneye/mediafiles.py
+++ motioneye/mediafiles.py
@@ -196,7 +196,7 @@ def _remove_older_files(directory, moment, to_clean_cloud, cloud_dir, camera_id,
                     logging.error('failed to remove %s: %s' % (dir_path, e))
 
     if to_clean_cloud and removed_folder_count > 0:
-        uploadservices.clean_cloud(camera_id, service_name, data, local_dir, cloud_dir)
+        uploadservices.clean_cloud(camera_id, service_name, {}, directory, cloud_dir)
 
 def find_ffmpeg():
     global _ffmpeg_binary_cache
