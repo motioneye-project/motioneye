0781e45b540be2ef84679daa91e59e6239cce0d2
bob-lee <bob.bumsuk.lee@gmail.com>
2019-04-02 12:38:37 +1300

    uploadservices.GooglePhoto

--- motioneye/uploadservices.py
+++ motioneye/uploadservices.py
@@ -497,6 +497,321 @@ class GoogleDrive(UploadService):
         return self._get_file_metadata(file_id)['title']
 
 
+class GooglePhoto(UploadService):
+    NAME = 'gphoto'
+
+    AUTH_URL = 'https://accounts.google.com/o/oauth2/auth'
+    TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
+
+    CLIENT_ID = '349038943026-m16svdadjrqc0c449u4qv71v1m1niu5o.apps.googleusercontent.com'
+    CLIENT_NOT_SO_SECRET = 'jjqbWmICpA0GvbhsJB3okX7s'
+
+    SCOPE = 'https://www.googleapis.com/auth/photoslibrary'
+    GOOGLE_PHOTO_API = 'https://photoslibrary.googleapis.com/v1/'
+    CHILDREN_URL = 'https://www.googleapis.com/drive/v2/files/%(parent_id)s/children?q=%(query)s'
+    CHILDREN_QUERY = "'%(parent_id)s' in parents and title = '%(child_name)s' and trashed = false"
+    UPLOAD_URL = 'https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart'
+    CREATE_FOLDER_URL = 'https://www.googleapis.com/drive/v2/files'
+
+    BOUNDARY = 'motioneye_multipart_boundary'
+
+    FOLDER_ID_LIFE_TIME = 300  # 5 minutes
+
+    def __init__(self, camera_id):
+        self._location = None
+        self._authorization_key = None
+        self._credentials = None
+        self._folder_ids = {}
+        self._folder_id_times = {}
+
+        UploadService.__init__(self, camera_id)
+
+    @classmethod
+    def get_authorize_url(cls):
+        query = {
+            'scope': cls.SCOPE,
+            'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob',
+            'response_type': 'code',
+            'client_id': cls.CLIENT_ID,
+            'access_type': 'offline'
+        }
+
+        return cls.AUTH_URL + '?' + urllib.urlencode(query)
+
+    def test_access(self):
+        try:
+            self._folder_ids = {}
+            #self._get_folder_id()
+            self._get_albums()
+            return True
+
+        except Exception as e:
+            return str(e)
+
+    def upload_data(self, filename, mime_type, data):
+        path = os.path.dirname(filename)
+        filename = os.path.basename(filename)
+
+        metadata = {
+            'title': filename,
+            'parents': [{'id': self._get_folder_id(path)}]
+        }
+
+        body = ['--' + self.BOUNDARY]
+        body.append('Content-Type: application/json; charset=UTF-8')
+        body.append('')
+        body.append(json.dumps(metadata))
+        body.append('')
+
+        body.append('--' + self.BOUNDARY)
+        body.append('Content-Type: %s' % mime_type)
+        body.append('')
+        body.append('')
+        body = '\r\n'.join(body)
+        body += data
+        body += '\r\n--%s--' % self.BOUNDARY
+
+        headers = {
+            'Content-Type': 'multipart/related; boundary="%s"' % self.BOUNDARY,
+            'Content-Length': len(body)
+        }
+
+        self._request(self.UPLOAD_URL, body, headers)
+
+    def dump(self):
+        return {
+            'location': self._location,
+            'credentials': self._credentials,
+            'authorization_key': self._authorization_key,
+        }
+
+    def load(self, data):
+        if data.get('location'):
+            self._location = data['location']
+            self._folder_ids = {}
+        if data.get('authorization_key'):
+            self._authorization_key = data['authorization_key']
+            self._credentials = None
+        if data.get('credentials'):
+            self._credentials = data['credentials']
+
+    def _get_folder_id(self, path=''):
+        now = time.time()
+
+        folder_id = self._folder_ids.get(path)
+        folder_id_time = self._folder_id_times.get(path, 0)
+
+        location = self._location
+        if not location.endswith('/'):
+            location += '/'
+
+        location += path
+
+        if not folder_id or (now - folder_id_time > self.FOLDER_ID_LIFE_TIME):
+            self.debug('finding folder id for location "%s"' % location)
+            folder_id = self._get_folder_id_by_path(location)
+
+            self._folder_ids[path] = folder_id
+            self._folder_id_times[path] = now
+
+        return folder_id
+
+    def _get_folder_id_by_path(self, path):
+        if path and path != '/':
+            path = [p.strip() for p in path.split('/') if p.strip()]
+            parent_id = 'root'
+            for name in path:
+                parent_id = self._get_folder_id_by_name(parent_id, name)
+
+            return parent_id
+
+        else:  # root folder
+            return self._get_folder_id_by_name(None, 'root')
+
+    def _get_folder_id_by_name(self, parent_id, child_name, create=True):
+        if parent_id:
+            query = self.CHILDREN_QUERY % {'parent_id': parent_id, 'child_name': child_name}
+            query = urllib.quote(query)
+
+        else:
+            query = ''
+
+        parent_id = parent_id or 'root'
+        # when requesting the id of the root folder, we perform a dummy request,
+        # event though we already know the id (which is "root"), to test the request
+
+        url = self.CHILDREN_URL % {'parent_id': parent_id, 'query': query}
+        response = self._request(url)
+        try:
+            response = json.loads(response)
+
+        except Exception:
+            self.error("response doesn't seem to be a valid json")
+            raise
+
+        if parent_id == 'root' and child_name == 'root':
+            return 'root'
+
+        items = response.get('items')
+        if not items:
+            if create:
+                self.debug('folder with name "%s" does not exist, creating it' % child_name)
+                self._create_folder(parent_id, child_name)
+                return self._get_folder_id_by_name(parent_id, child_name, create=False)
+
+            else:
+                msg = 'folder with name "%s" does not exist' % child_name
+                self.error(msg)
+                raise Exception(msg)
+
+        return items[0]['id']
+
+    def _create_folder(self, parent_id, child_name):
+        metadata = {
+            'title': child_name,
+            'parents': [{'id': parent_id}],
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+
+        body = json.dumps(metadata)
+
+        headers = {
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+
+        self._request(self.CREATE_FOLDER_URL, body, headers)
+
+    def _get_albums(self):
+        response = self._request(self.GOOGLE_PHOTO_API + 'albums')
+        try:
+            response = json.loads(response)
+
+        except Exception:
+            self.error("response doesn't seem to be a valid json")
+            raise
+
+        albums = response.get('albums')
+        self.debug('got %s album(s)' % len(albums))
+        return albums
+
+    def _request(self, url, body=None, headers=None, retry_auth=True, method=None):
+        if not self._credentials:
+            if not self._authorization_key:
+                msg = 'missing authorization key'
+                self.error(msg)
+                raise Exception(msg)
+
+            self.debug('requesting credentials')
+            try:
+                self._credentials = self._request_credentials(self._authorization_key)
+                self.save()
+
+            except Exception as e:
+                self.error('failed to obtain credentials: %s' % e)
+                raise
+
+        headers = headers or {}
+        headers['Authorization'] = 'Bearer %s' % self._credentials['access_token']
+
+        self.debug('requesting %s' % url)
+        request = urllib2.Request(url, data=body, headers=headers)
+        if method:
+            request.get_method = lambda: method
+        try:
+            response = utils.urlopen(request)
+
+        except urllib2.HTTPError as e:
+            if e.code == 401 and retry_auth:  # unauthorized, access token may have expired
+                try:
+                    self.debug('credentials have probably expired, refreshing them')
+                    self._credentials = self._refresh_credentials(self._credentials['refresh_token'])
+                    self.save()
+
+                    # retry the request with refreshed credentials
+                    return self._request(url, body, headers, retry_auth=False)
+
+                except Exception:
+                    self.error('refreshing credentials failed')
+                    raise
+
+            else:
+                try:
+                    e = json.load(e)
+                    msg = e['error']['message']
+
+                except Exception:
+                    msg = str(e)
+
+                self.error('request failed: %s' % msg)
+                raise Exception(msg)
+
+        except Exception as e:
+            self.error('request failed: %s' % e)
+            raise
+
+        return response.read()
+
+    def _request_credentials(self, authorization_key):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        body = {
+            'code': authorization_key,
+            'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob',
+            'client_id': self.CLIENT_ID,
+            'client_secret': self.CLIENT_NOT_SO_SECRET,
+            'scope': self.SCOPE,
+            'grant_type': 'authorization_code'
+        }
+        body = urllib.urlencode(body)
+
+        request = urllib2.Request(self.TOKEN_URL, data=body, headers=headers)
+
+        try:
+            response = utils.urlopen(request)
+
+        except urllib2.HTTPError as e:
+            error = json.load(e)
+            raise Exception(error.get('error_description') or error.get('error') or str(e))
+
+        data = json.load(response)
+
+        return {
+            'access_token': data['access_token'],
+            'refresh_token': data['refresh_token']
+        }
+
+    def _refresh_credentials(self, refresh_token):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        body = {
+            'refresh_token': refresh_token,
+            'client_id': self.CLIENT_ID,
+            'client_secret': self.CLIENT_NOT_SO_SECRET,
+            'grant_type': 'refresh_token'
+        }
+        body = urllib.urlencode(body)
+
+        request = urllib2.Request(self.TOKEN_URL, data=body, headers=headers)
+
+        try:
+            response = utils.urlopen(request)
+
+        except urllib2.HTTPError as e:
+            error = json.load(e)
+            raise Exception(error.get('error_description') or error.get('error') or str(e))
+
+        data = json.load(response)
+
+        return {
+            'access_token': data['access_token'],
+            'refresh_token': data.get('refresh_token', refresh_token)
+        }
+
+
 class Dropbox(UploadService):
     NAME = 'dropbox'
 
