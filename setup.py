e57a2fd6677c51bda89c20f031abdba98ae609e1
Calin Crisan <ccrisan@gmail.com>
2019-04-20 12:49:05 +0300

    setup.py: limit tornado version to <6

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
 
     packages=['motioneye'],
 
-    install_requires=['tornado>=3.1', 'jinja2', 'pillow', 'pycurl'],
+    install_requires=['tornado>=3.1,<6', 'jinja2', 'pillow', 'pycurl'],
 
     package_data={
         'motioneye': [
