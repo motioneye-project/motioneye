b647dc04ca790b1920bff3f8bb24d92ba98e8e20
Vlsarro <drekmaro@gmail.com>
2020-03-13 16:20:01 +0300

    python 2 to 3, remove old file opening

--- motioneye/server.py
+++ motioneye/server.py
@@ -68,9 +68,9 @@ class Daemon(object):
         # redirect standard file descriptors
         sys.stdout.flush()
         sys.stderr.flush()
-        si = file('/dev/null', 'r')
-        so = file('/dev/null', 'a+')
-        se = file('/dev/null', 'a+', 0)
+        si = open('/dev/null', 'r')
+        so = open('/dev/null', 'a+')
+        se = open('/dev/null', 'a+', 0)
         os.dup2(si.fileno(), sys.stdin.fileno())
         os.dup2(so.fileno(), sys.stdout.fileno())
         os.dup2(se.fileno(), sys.stderr.fileno())
