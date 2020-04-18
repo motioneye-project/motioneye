01a49d54d2f6154854e5862f92cb706270be68bd
Vlsarro <drekmaro@gmail.com>
2020-04-18 16:43:56 +0300

    add test of successful login

--- tests/test_handlers/test_login.py
+++ tests/test_handlers/test_login.py
@@ -8,11 +8,18 @@ from tests.test_handlers import HandlerTestCase
 class LoginHandlerTest(HandlerTestCase):
     handler_cls = LoginHandler
 
-    def test_get(self):
+    def test_get_login_no_params(self):
         response = self.fetch('/login')
+        self.assertEqual(200, response.code)
         self.assertEqual({}, json.loads(response.body))
 
-    def test_get_invalid_admin_auth(self):
+    def test_get_login_success(self):
+        url = '/login/?_=1587216975186&_username=admin&_login=true&_signature=f430e0da555b7714792e9cf9553c22536d00cfc0'
+        response = self.fetch(url)
+        self.assertEqual(200, response.code)
+        self.assertEqual({}, json.loads(response.body))
+
+    def test_get_login_fail(self):
         response = self.fetch('/login?_admin=true')
         self.assertEqual(403, response.code)
         self.assertEqual('application/json', response.headers.get('Content-Type'))
