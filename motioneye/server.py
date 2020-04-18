7c42d44d09615bed813d71e6c255c06fec1b1600
Vlsarro <drekmaro@gmail.com>
2020-04-18 10:19:17 +0300

    remove old check of running loop

--- motioneye/server.py
+++ motioneye/server.py
@@ -315,8 +315,6 @@ def start_motion():
 
     # add a motion running checker
     def checker():
-        if io_loop._stopped:
-            return
 
         if not motionctl.running() and motionctl.started() and config.get_enabled_local_motion_cameras():
             try:
