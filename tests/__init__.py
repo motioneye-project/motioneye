a51ab80dc0e07307466bd87f6fab64543aaac41c
Vlsarro <drekmaro@gmail.com>
2020-04-19 08:34:18 +0300

    add basic tests for "test_mjpeg_url" (does not cover a lot of cases)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,11 @@
 from unittest import mock
 
+from tornado.web import Application
+from tornado.testing import AsyncHTTPTestCase
+
+
+__all__ = ('AsyncMock', 'WebTestCase')
+
 
 class AsyncMock(mock.MagicMock):
 
@@ -12,3 +18,18 @@ class AsyncMock(mock.MagicMock):
 
     def __await__(self):
         return self().__await__()
+
+
+class WebTestCase(AsyncHTTPTestCase):
+
+    handler = None
+
+    def get_app(self):
+        self.app = Application(self.get_handlers(), **self.get_app_kwargs())
+        return self.app
+
+    def get_handlers(self):
+        return [('/', self.handler)]
+
+    def get_app_kwargs(self):
+        return {}
---
+++ tests/test_utils/test_mjpeg.py
@@ -0,0 +1,95 @@
+import tornado.testing
+from tornado.web import RequestHandler
+
+from tests import WebTestCase
+from motioneye.utils.mjpeg import test_mjpeg_url
+
+
+class UtilsMjpegTest(WebTestCase):
+
+    def setUp(self) -> None:
+        super(UtilsMjpegTest, self).setUp()
+        self.data = None
+
+    def get_handlers(self):
+        test = self
+
+        class MjpegHandler(RequestHandler):
+            async def get(self):
+                if 'image/jpeg' in test.data:
+                    self.set_header('Content-Type', 'image/jpeg')
+
+                if 'mjpeg' in test.data:
+                    self.set_header('Content-Type', 'multipart/x-mixed-replace')
+                self.write(test.data)
+                await self.flush()
+
+        return [('/', MjpegHandler)]
+
+    def test_test_mjpeg_url_invalid_data(self):
+        self.data = 'Some random string'
+
+        callback_result = []
+
+        def mock_on_response(cameras=None, error=None) -> None:
+            self.stop()
+            callback_result.append((cameras, error))
+
+        test_mjpeg_url({'port': self.get_http_port()}, auth_modes=['basic'], allow_jpeg=True, callback=mock_on_response)
+
+        self.wait()
+        self.assertEqual(1, len(callback_result))
+        self.assertIsNone(callback_result[0][0])
+        self.assertEqual('not a supported network camera', callback_result[0][1])
+
+    def test_test_mjpeg_url_jpeg_cam(self):
+        self.data = 'image/jpeg camera'
+        callback_result = []
+
+        def mock_on_response(cameras=None, error=None) -> None:
+            self.stop()
+            callback_result.append((cameras, error))
+
+        test_mjpeg_url({'port': self.get_http_port()}, auth_modes=['basic'], allow_jpeg=True, callback=mock_on_response)
+
+        self.wait()
+        self.assertEqual(1, len(callback_result))
+        self.assertIsNone(callback_result[0][1])
+
+        cams = callback_result[0][0]
+        self.assertEqual(1, len(cams))
+
+        cam = cams[0]
+        self.assertDictEqual({
+            'id': 1,
+            'name': 'JPEG Network Camera',
+            'keep_alive': True
+        }, cam)
+
+    def test_test_mjpeg_url_mjpeg_cam(self):
+        self.data = 'mjpeg camera'
+        callback_result = []
+
+        def mock_on_response(cameras=None, error=None) -> None:
+            self.stop()
+            callback_result.append((cameras, error))
+
+        test_mjpeg_url({'port': self.get_http_port()}, auth_modes=['basic'], allow_jpeg=True, callback=mock_on_response)
+
+        self.wait()
+        self.assertEqual(1, len(callback_result))
+        self.assertIsNone(callback_result[0][1])
+
+        cams = callback_result[0][0]
+        self.assertEqual(1, len(cams))
+
+        cam = cams[0]
+        self.assertDictEqual({
+            'id': 1,
+            'name': 'MJPEG Network Camera',
+            'keep_alive': True
+        }, cam)
+
+
+if __name__ == '__main__':
+    tornado.testing.main()
