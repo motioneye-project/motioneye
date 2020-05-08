99cd37dd5cb79e67132fe8da61f93f0aadb44e2c
mmeah <mmeah@office.mmeah.com>
2020-05-08 18:21:15 -0500

    Insert py file to sys path first to avoid confusing second instance folder with one installed in python directory

--- motioneye/meyectl.py
+++ motioneye/meyectl.py
@@ -23,7 +23,7 @@ import pipes
 import sys
 
 # make sure motioneye is on python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import settings
 
