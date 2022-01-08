f3691edbaf97bbd55a6d27fb706eea2520c329d3
Jean Michault <jmichault@users.sourceforge.net>
2022-01-08 13:53:22 +0100

    korekto de la listo de rezolucioj v4l

--- motioneye/controls/v4l2ctl.py
+++ motioneye/controls/v4l2ctl.py
@@ -92,39 +92,15 @@ def list_resolutions(device):
     resolutions = set()
     output = b''
     started = time.time()
-    cmd = 'v4l2-ctl -d %(device)s --list-formats-ext | grep -vi stepwise | grep -oE "[0-9]+x[0-9]+" || true' % {
-            'device': pipes.quote(device)}
+    cmd = 'v4l2-ctl -d %(device)s --list-formats-ext | grep -vi stepwise | grep -oE "[0-9]+x[0-9]+" || true'
+    actual_cmd = cmd % { 'device': pipes.quote(device)}
+    logging.debug('running command "%s"' % actual_cmd)
 
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, bufsize=1)
-
-    fd = p.stdout.fileno()
-    fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-    fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-
-    while True:
-        data = p.stdout.read(1024)
-        if data == b'':
-            break
-        if not data:
-            data = ''
-            time.sleep(0.01)
-        else:
-            output += data
-
-        if len(output) > 10240:
-            logging.warning('v4l2-ctl command returned more than 10k of output')
-            break
-        
-        if time.time() - started > _V4L2_TIMEOUT:
-            logging.warning('v4l2-ctl command ran for more than %s seconds' % _V4L2_TIMEOUT)
-            break
-    
     try:
-        # try to kill the v4l2-ctl subprocess
-        p.kill()
-    
+        output = utils.call_subprocess(actual_cmd, shell=True, stderr=subprocess.STDOUT)
     except:
-        pass  # nevermind
+        logging.error('failed to list resolutions of device %(device)s...' % {'device': device})
+
     output = utils.make_str(output)
 
     for pair in output.split('\n'):
