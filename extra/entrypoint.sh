daeb21f51a5ca54a0dec52eb3e07df3df8b19dc0
MichaIng <micha@dietpi.com>
2022-03-15 20:08:00 +0100

    Docker workflow enhancements
    Add --skip-systemd command-line option to motioneye_init command to skip the systemd service setup.
    
    Use motioneye_init in the Docker workflow and skip all package installs which are done via motioneye_init or via pip already.
    
    Further merge some Dockerfile steps.
    
    Signed-off-by: MichaIng <micha@dietpi.com>

--- extra/entrypoint.sh
+++ extra/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
-test -e /etc/motioneye/motioneye.conf || \
-cp /usr/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf ; \
 # We need to chown at startup time since volumes are mounted as root. This is fugly.
-mkdir -p /var/run/motioneye /var/log/motioneye ; \
-chown motion:motion /var/run /var/run/motioneye /var/log /etc/motioneye /var/lib/motioneye /var/log/motioneye /usr/share/motioneye/extra ; \
-su -g motion motion -s /bin/bash -c "LANGUAGE=en /usr/local/bin/meyectl startserver -c /etc/motioneye/motioneye.conf"
+mkdir -p /run/motioneye
+chown motion:motion /run/motioneye
+[ -f '/etc/motioneye/motioneye.conf' ] || cp -a /etc/motioneye.conf.sample /etc/motioneye/motioneye.conf
+exec su -g motion motion -s /bin/dash -c "LANGUAGE=en exec /usr/local/bin/meyectl startserver -c /etc/motioneye/motioneye.conf"
