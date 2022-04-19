8f6c24f2ebe103f66a9c7c257ef0d19c0c770d91
sirdel <3763715+sirdel@users.noreply.github.com>
2022-04-19 22:44:12 +0200

    Update motioneye/sendtelegram.py
    Co-authored-by: MichaIng <micha@dietpi.com>

--- motioneye/sendtelegram.py
+++ motioneye/sendtelegram.py
@@ -150,7 +150,6 @@ def main(parser, args):
     options = parse_options(parser, args)
     meyectl.configure_logging('telegram', options.log_to_file)
     logging.debug(options)
-    
     message = 'Motion has been detected by camera "%(camera)s/%(hostname)s" at %(moment)s (%(timezone)s).'
 
     # do not wait too long for media list,
