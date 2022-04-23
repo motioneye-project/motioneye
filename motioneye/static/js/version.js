fddcace825617db55d96f06de330f16af8c0c243
MichaIng <micha@dietpi.com>
2022-04-23 18:40:40 +0200

    Update deprecated jQuery .click() shorthand
    .click(fn) => .on('click', fn)
    .click() => .trigger('click')
    
    And further cases of .load(fn) and .error(fn).
    
    Signed-off-by: MichaIng <micha@dietpi.com>

--- motioneye/static/js/version.js
+++ motioneye/static/js/version.js
@@ -1,5 +1,5 @@
 
-$(window).load(function () {
+$(window).on('load', function () {
     if (window.parent && window.parent.postMessage) {
         window.parent.postMessage({'hostname': hostname, 'version': version, 'url': window.location.href.replace('version/', '')}, '*');
     }
