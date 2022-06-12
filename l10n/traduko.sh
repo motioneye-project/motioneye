3266ea3444d30ac5930deed13b44a1d8a60c0881
jmichault <jmt.2000@free.fr>
2022-06-12 16:00:32 +0200

    better handle \ character in strings to translate

--- l10n/traduko.sh
+++ l10n/traduko.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 ################################################################
 # skripto por aŭtomate traduki frazon
 ################################################################
 DEBUG=
-#DEBUG=1
 
 src=$1
 dst=$2
 txt=$3
+#txt="${3//\\/\\\\}"
 
 cook=$(find . -maxdepth 1 _traduko.jar -mmin -15 2>/dev/null)
 
@@ -25,7 +25,7 @@ fi
 MSG0=$(curl -b _traduko.jar -A 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' \
   --refer 'https://translate.google.com/' \
   "https://translate.google.com/translate_a/single?client=webapp&sl=${src}&tl=${dst}&hl=${dst}&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=gt&pc=1&otf=1&ssel=0&tsel=0&kc=1&tk=&ie=UTF-8&oe=UTF-8" \
-  --data-urlencode "q=${txt}" 2>/dev/null \
+  --data-urlencode "q=${txt//\\/\\}" 2>/dev/null \
 )
 
 [ "$DEBUG" ] && echo "$src txt=$txt" >&2
