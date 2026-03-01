9de7ef324c97dd128d4eaa8ee3a71b4423005f3b
strawberry-poodle <swe.study.anonymous.3215435325@gmail.com>
2026-03-01 18:26:20 +0200

    Improve PR with recommended changes

--- motioneye/mediafiles.py
+++ motioneye/mediafiles.py
@@ -23,7 +23,6 @@ import os.path
 import re
 import subprocess
 import typing
-import math
 from errno import EAGAIN, ENOENT
 from hashlib import sha1
 from io import BytesIO
@@ -425,14 +424,14 @@ def cleanup_media(media_type: str) -> None:
         )
 
 def get_movie_duration_seconds(path):
-    cmd = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {path}'
+    cmd = ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', path]
     
     try:
         result = utils.call_subprocess(cmd.split(), stderr=subprocess.STDOUT)
-        duration_seconds = math.floor(float(result))
+        duration_seconds = int(float(result))
         return duration_seconds
     
-    except subprocess.CalledProcessError as e:
+    except Exception as e:
         logging.error(f'failed to determine duration of {path}: {e}')
 
         return 0
@@ -444,10 +443,7 @@ def make_movie_preview(camera_config: dict, full_path: str) -> typing.Union[str,
     offs = pre_capture / framerate
     movie_duration = get_movie_duration_seconds(full_path)
     
-    if(movie_duration >= 4):  
-        offs = max(4, offs * 2)
-    else:
-        offs = offs * 2
+    offs = max((x for x in {offs*2, offs} if x <= movie_duration), default=movie_duration/2)
         
     path = quote(full_path)
     thumb_path = full_path + '.thumb'
