cbb65e620a6b2b892da2effbec6ba9fa60e65962
Marijn <168113859+Marijn0@users.noreply.github.com>
2026-08-30 22:01:43 +0200

    add tests for the session lifetime

--- tests/test_handlers/test_login.py
+++ tests/test_handlers/test_login.py
@@ -1,11 +1,14 @@
 import json
+from email.utils import parsedate_to_datetime
 from hashlib import sha1
+from http.cookies import SimpleCookie
 from unittest.mock import patch
 
 import tornado.testing
 from argon2 import PasswordHasher
 
-from motioneye import config
+from motioneye import config, settings
+from motioneye.handlers.base import _session_store
 from motioneye.handlers.login import LoginHandler
 from tests.test_handlers import HandlerTestCase
 
@@ -121,6 +124,86 @@ class LoginHandlerTest(HandlerTestCase):
                 )
                 self.assertEqual(200, response.code)
                 mock_set_normal.assert_called_once_with(normal_plain)
+
+    def _assert_session_lifetime(
+        self, response, user_type: str, expected_seconds: int
+    ) -> None:
+        response_date = parsedate_to_datetime(response.headers['Date'])
+        cookie = SimpleCookie()
+        cookie.load(response.headers.get('Set-Cookie', ''))
+        cookie_expires = parsedate_to_datetime(cookie['user']['expires'])
+
+        self.assertAlmostEqual(
+            expected_seconds,
+            (cookie_expires - response_date).total_seconds(),
+            delta=1,
+        )
+
+        self.assertEqual(1, len(_session_store))
+        session = next(iter(_session_store.values()))
+        self.assertEqual(user_type, session['user'])
+        self.assertAlmostEqual(cookie_expires.timestamp(), session['expires'], delta=1)
+
+    def test_normal_session_lifetime_configurable(self):
+        normal_user = 'user'
+        normal_pass = 'watcher'
+        main_config = {
+            '@admin_username': 'admin',
+            '@admin_password': ph.hash('adminpass'),
+            '@normal_username': normal_user,
+            '@normal_password': ph.hash(normal_pass),
+        }
+        with patch.object(settings, 'NORMAL_SESSION_EXPIRY_HOURS', 5):
+            with patch.object(config, '_main_config_cache', main_config):
+                response = self.fetch(
+                    '/login',
+                    method='POST',
+                    body=f'username={normal_user}&password={normal_pass}',
+                    headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                )
+        self.assertEqual(200, response.code)
+        self._assert_session_lifetime(response, 'normal', 5 * 3600)
+
+    def test_admin_session_lifetime_not_configurable(self):
+        admin_user = 'admin'
+        admin_pass = 's3cret'
+        main_config = {
+            '@admin_username': admin_user,
+            '@admin_password': ph.hash(admin_pass),
+            '@normal_username': '',
+            '@normal_password': '',
+        }
+        # even a large normal-user lifetime must not affect the admin cookie
+        with patch.object(settings, 'NORMAL_SESSION_EXPIRY_HOURS', 999):
+            with patch.object(config, '_main_config_cache', main_config):
+                response = self.fetch(
+                    '/login',
+                    method='POST',
+                    body=f'username={admin_user}&password={admin_pass}',
+                    headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                )
+        self.assertEqual(200, response.code)
+        self._assert_session_lifetime(response, 'admin', 24 * 3600)
+
+    def test_normal_session_lifetime_minimum_clamp(self):
+        normal_user = 'user'
+        normal_pass = 'watcher'
+        main_config = {
+            '@admin_username': 'admin',
+            '@admin_password': ph.hash('adminpass'),
+            '@normal_username': normal_user,
+            '@normal_password': ph.hash(normal_pass),
+        }
+        with patch.object(settings, 'NORMAL_SESSION_EXPIRY_HOURS', 0):
+            with patch.object(config, '_main_config_cache', main_config):
+                response = self.fetch(
+                    '/login',
+                    method='POST',
+                    body=f'username={normal_user}&password={normal_pass}',
+                    headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                )
+        self.assertEqual(200, response.code)
+        self._assert_session_lifetime(response, 'normal', 1 * 3600)
 
     def test_logout(self):
         cookie = self.make_session_cookie('admin')
