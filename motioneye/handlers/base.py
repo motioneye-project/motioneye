4462009e8343b4b79f119dea0c55daccab7cd23f
Marijn <168113859+Marijn0@users.noreply.github.com>
2026-09-02 17:46:27 +0200

    review suggestion
    Co-authored-by: MichaIng <micha@dietpi.com>

--- motioneye/handlers/base.py
+++ motioneye/handlers/base.py
@@ -36,7 +36,7 @@ _SESSION_EXPIRY_SECONDS: int = 86400
 _session_store: dict = {}
 
 
-def session_expiry_seconds(user_type):
+def session_expiry_seconds(user_type: str) -> int:
     if user_type == 'normal':
         return max(1, settings.NORMAL_SESSION_EXPIRY_HOURS) * 3600
     return _SESSION_EXPIRY_SECONDS
